206ed81aa1c325d93c7e32e0e451fb580b21884e
abram.D03 <abram.wang@outlook.com>
2024-07-28 09:08:12 +0000

    更新 101 demo

--- 101_alpha.py
+++ 101_alpha.py
@@ -1,16 +1,16 @@
 import model.operator as op
-
+import pymoneta.data as data
 
 class Alphas(object):
     def __init__(self, df_data):
 
-        self.open = df_data['S_DQ_OPEN'] 
-        self.high = df_data['S_DQ_HIGH'] 
-        self.low = df_data['S_DQ_LOW']   
-        self.close = df_data['S_DQ_CLOSE'] 
-        self.volume = df_data['S_DQ_VOLUME']*100 
-        self.returns = df_data['S_DQ_PCTCHANGE'] 
-        self.vwap = (df_data['S_DQ_AMOUNT']*1000)/(df_data['S_DQ_VOLUME']*100+1) 
+        self.open = df_data['open'] 
+        self.high = df_data['high'] 
+        self.low = df_data['low']   
+        self.close = df_data['close'] 
+        self.volume = df_data['volume']
+        self.returns = df_data['rtn'] 
+        self.vwap = (df_data['total_turnover'])/(df_data['volume']+1) 
         
     # Alpha#1	 (rank(Ts_ArgMax(SignedPower(((returns < 0) ? stddev(returns, 20) : close), 2.), 5)) -0.5)
     def alpha001(self):
@@ -20,6 +20,15 @@ class Alphas(object):
 
 
 def main():
+    df = data.GetDayBar("stock", "*", "20210101", "20210131")
+    df["rtn"] = (df["close"] - df["prev_close"])/df["prev_close"]
+
+    #print(df)
+    #print(df.columns)
+    #return
+    stock=Alphas(df)
+    df['alpha001']=stock.alpha001() 
+    print(df['alpha001'])
     pass
 
 
